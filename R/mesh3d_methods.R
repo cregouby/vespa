2400e88d5dee25428c8160cf9064d275e81999e4
Christophe Regouby <christophe.regouby@airbus.com>
2026-06-26 13:57:15 +0200

    improve rendering of summary and print

--- R/mesh3d_methods.R
+++ R/mesh3d_methods.R
@@ -3,7 +3,11 @@
 print.mesh3d <- function(x, ...) {
   nv <- if (!is.null(x$vb)) ncol(x$vb) else 0L
   nf <- if (!is.null(x$it)) ncol(x$it) else 0L
-  cat("<mesh3d>  ", nv, " vertices  ", nf, " faces\n", sep = "")
+  cli::cli_text(
+    "{.cls mesh3d}  \
+     {.val {nv}} {cli::qty(nv)}{?vertex/vertices}  \
+     {.val {nf}} {cli::qty(nf)}{?face/faces}"
+  )
   invisible(x)
 }
 
@@ -17,13 +21,13 @@ summary.mesh3d <- function(object, ...) {
   diag_len <- if (nv > 0L) sqrt(sum((bb[2L, ] - bb[1L, ])^2)) else NA_real_
   structure(
     list(
-      n_vertices   = nv,
-      n_faces      = nf,
-      xrange       = bb[, 1L],
-      yrange       = bb[, 2L],
-      zrange       = bb[, 3L],
-      diagonal     = diag_len,
-      has_normals  = !is.null(object$normals)
+      n_vertices  = nv,
+      n_faces     = nf,
+      xrange      = bb[, 1L],
+      yrange      = bb[, 2L],
+      zrange      = bb[, 3L],
+      diagonal    = diag_len,
+      has_normals = !is.null(object$normals)
     ),
     class = "summary.mesh3d"
   )
@@ -32,13 +36,19 @@ summary.mesh3d <- function(object, ...) {
 #' @method print summary.mesh3d
 #' @export
 print.summary.mesh3d <- function(x, ...) {
-  cat("mesh3d summary\n")
-  cat("  Vertices :", x$n_vertices, "\n")
-  cat("  Faces    :", x$n_faces, "\n")
-  cat("  X range  :", x$xrange[1L], "-", x$xrange[2L], "\n")
-  cat("  Y range  :", x$yrange[1L], "-", x$yrange[2L], "\n")
-  cat("  Z range  :", x$zrange[1L], "-", x$zrange[2L], "\n")
-  cat("  Diagonal :", round(x$diagonal, 4L), "\n")
-  cat("  Normals  :", x$has_normals, "\n")
+  fmt_range <- function(r) sprintf("[%g, %g]", signif(r[1L], 5L), signif(r[2L], 5L))
+  normals_entry <- if (x$has_normals) "{.field yes}" else cli::style_dim("no")
+  cli::cli_h3("mesh3d")
+  cli::cli_dl(c(
+    "Vertices" = "{.val {x$n_vertices}}",
+    "Faces"    = "{.val {x$n_faces}}"
+  ))
+  cli::cli_dl(c(
+    "X range"  = fmt_range(x$xrange),
+    "Y range"  = fmt_range(x$yrange),
+    "Z range"  = fmt_range(x$zrange),
+    "Diagonal" = "{.val {signif(x$diagonal, 5L)}}"
+  ))
+  cli::cli_dl(c("Normals" = normals_entry))
   invisible(x)
 }
